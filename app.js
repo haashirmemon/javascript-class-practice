64a4e20d44060c197583a9b2981313059f162606
haashirmemon <haashirmemon6@gmail.com>
2024-03-02 00:00:43 -0800

    class practice

---
+++ app.js
@@ -0,0 +1,58 @@
+// var num1= +prompt("enter your number")
+// var num2= +prompt("enter your secondnumber")
+// var num3= +prompt("enter your thirdnumber")
+// var num4= +prompt("enter your fourthnumber")
+// console.log(num1+num2+num3+num4)
+
+// var num1= +prompt("90")
+// var num2= +prompt("10")
+// console.log(num1==num2)    
+// console.log(10 !== 20); 
+// document.write(hello);
+
+
+
+// var percentage = +prompt ("what is your percentage")
+// console.log('50')
+// if else(percentage < 90 )
+// else(console.log(percentage))
+
+// var num1= +prompt("100")
+// ifelse(divisible by 2 )(console.log(true))
+// ifelse(odd)(console.log(false)
+
+
+// function sum(num1 ,num2){
+//     console.log("sum of two numbers===>"  )
+// }
+
+// console.log("number")
+
+// sum2("03363098433")
+
+
+
+
+//      function calcolation (){
+//     console.log("hello world")
+//     return "105"
+// }
+
+var h1 = document.
+querySelector("#h1")
+console.log(h1.innerHTML)
+var paragraph = document.
+querySelector("#para")
+console.log(paragraph.innerHTML)
+ 
+
+function changetext(){
+if(paragraph.innerHTML=== "changed through js" & h1.innerHTML ==="changed through js"){
+    h1.innerHTML="hello world"
+    paragraph.innerHTML="lorem ipsum"
+}
+else{
+    paragraph.innerHTML="changed through js "
+    h1.innerHTML="changed through js"
+}
+}
