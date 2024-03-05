834658486838496f4459b32a93737419f234dd28
haashirmemon <haashirmemon6@gmail.com>
2024-03-04 22:41:41 -0800

    updated

--- app.js
+++ app.js
@@ -38,21 +38,115 @@
 //     return "105"
 // }
 
-var h1 = document.
-querySelector("#h1")
-console.log(h1.innerHTML)
-var paragraph = document.
-querySelector("#para")
-console.log(paragraph.innerHTML)
- 
+// var h1 = document.
+// querySelector("#h1")
+// console.log(h1.innerHTML)
+// var paragraph = document.
+// querySelector("#para")
+// console.log(paragraph.innerHTML)
 
-function changetext(){
-if(paragraph.innerHTML=== "changed through js" & h1.innerHTML ==="changed through js"){
-    h1.innerHTML="hello world"
-    paragraph.innerHTML="lorem ipsum"
-}
-else{
-    paragraph.innerHTML="changed through js "
-    h1.innerHTML="changed through js"
-}
-}
+
+// function changetext(){
+// if(paragraph.innerHTML=== "changed through js" & h1.innerHTML ==="changed through js"){
+//     h1.innerHTML="hello world"
+//     paragraph.innerHTML="lorem ipsum"
+// }
+// else{
+//     paragraph.innerHTML="changed through js "
+//     h1.innerHTML="changed through js"
+// }
+// }
+
+// var name= ("haashir")
+// // console.log(name)
+// alert("hello world")
+// prompt("haashir")
+
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+
+// function bewakuf(params) {
+// console.log(name)
+// console.log(name)
+// console.log(name)
+// console.log(name)
+
+// }
+
+// bewakuf()
+// bewakuf()
+
+// function nalaik(params) {
+
+//     console.log("hello world")
+
+// }
+
+// nalaik(
+// )
+
+// undefined
+// null
+
+
+
+// var  username
+// console.log(username)
+
+
+// function greetuser (  username , lastname ,age) {
+// console.log("hello ,"+ username,+lastname,+age)
+
+// }
+
+// greetuser("haashir" ,"shakeel" ,"18")
+
+
+// function sum(num1 ,num2) {
+//     console.log("sum of two numbers===>",num1 + num2 )
+//     sum(50,70)
+
+// }
+// var lastname ="memon"
+
+// function greetuser() {
+//     varusername= "haashir"
+//     console.log(username + lastname)
+
+// }
+
+// console.log(username)
+// console.log(lastname)
+
+// greetuser()
+
+
+
+var HEAD = document.querySelector(".HEAD");
+// HEAD.innerHTML = "change through js"
+
+// function changetext(head) {
+
+    //     var HEAD = document.querySelector(".HEAD");
+    // HEAD.innerHTML = "change through js"
+//     if (HEAD.innerHTML = "change through js") {
+//         HEAD.innerHTML = "change through js"
+//     }
+//     //     var HEAD = document.querySelector(".HEAD");
+//     else {
+//         HEAD.innerHTML = "change through js"
+//     }
+
+// }
+
+var username="haashir"
+var lastname="shakeel"
+var age="18"
+
+console.log(`my name is ${firstname} ${lastname} and my age is ${age}`)
+
+
+
