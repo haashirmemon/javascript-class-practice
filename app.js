a862ee1141e43fc34bab50f12b946a9ab0baa097
haashirmemon <haashirmemon6@gmail.com>
2024-03-29 02:48:46 -0700

    fileupdated

--- app.js
+++ app.js
@@ -130,8 +130,8 @@
 
 // function changetext(head) {
 
-    //     var HEAD = document.querySelector(".HEAD");
-    // HEAD.innerHTML = "change through js"
+//     var HEAD = document.querySelector(".HEAD");
+// HEAD.innerHTML = "change through js"
 //     if (HEAD.innerHTML = "change through js") {
 //         HEAD.innerHTML = "change through js"
 //     }
@@ -158,15 +158,15 @@
 //      console.log("input2") 
 //      console.log("input3")
 //       console.log("input4") ;
-    
+
 // }
 
 
 // greetuser();
 
 
-var head = document.querySelector(".HEAD")
-var paragraph = document.querySelector("#para")
+// var head = document.querySelector(".HEAD")
+// var paragraph = document.querySelector("#para")
 
 
 
@@ -177,7 +177,7 @@ var paragraph = document.querySelector("#para")
 
 //     var head = document.querySelector(".HEAD")
 //     var paragraph = document.querySelector("#para")
-    
+
 
 //     head.innerHTML = "change through js"
 // paragraph.innerHTML = "lorem ipsum"
@@ -282,11 +282,186 @@ var paragraph = document.querySelector("#para")
 
 // let condition =false |10 !='10'|| 10!=='10' ;
 // console.log(condition);
-let num = 34
- num = 20
-console.log(num);
+// let num = 34
+//  num = 20
+// console.log(num);
+// const num= []
+// console.log(typeof (num));
 
 
+// fruits.splice(1,0,'gvava','nashpati',, 'falsa','anaar')
 
 
+// fruits.pop()
 
+// console.log(fruits.includes('banana'));
+// console.log(fruits.indexOf('apple'));
+// let a = 5
+// for (let; i < 1000; i++) {
+//    console.log(a + i);
+
+// }
+// let obj = {
+//     name : 'haashir',
+//     role :'web developer',
+//     company:'haashirs machinery'
+// }
+
+// for (const key in obj){
+//        console.log(key,obj);
+// }
+// for (const h of'haashir') {
+//     console.log(h);
+// }
+// console.log('hello world');
+// for (let i= 2; i <=100; i++) {
+//    console.log('hello world',i);
+//    i++
+// }
+// console.log('loop kai bahar');
+
+// const userinput =+prompt('enter table number')
+// const times =  +prompt('how many times do you want')
+
+// for (let i = 1; i <= times; i++) {
+//     console.log (`${userinput} * ${i} = ${userinput*i}`);
+// }
+
+// const userInput = +prompt('enter table number')
+// const times = +prompt('How many time do you want?')
+
+// for (let i = 1; i <= times; i++) {
+//     console.log(`${userInput} * ${i} = ${userInput*i}`);
+// }
+// Infinite loop
+// for (let i = 1; i >0; i++) {
+//     console.log(a);;
+// }
+// for (let i = 1; i > 0; i++) {
+//     console.log(i);
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log('outer Loop ===> ' , i);
+//     for(let j = 1; j <= 5; j++){
+//         console.log('inner Loop===>' , j);
+//     }
+// }
+// for (let i = 1; i <= 100 ; i++) {
+//    console.log('outer loop===>',i);
+//    for (let j = 1; j <= 5; j++) {
+//    console.log('inner loop===>',j);;
+//    }
+// }
+
+// for (let i = 1; i <= 100; i++) {
+//     // console.log(i);
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(`${i} * ${j} = ${i*j}`);
+//     }
+// }
+
+// for (let i = 1; i < 10; i++) {
+//     console.log(i);
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(`${i} * ${j} = ${i*j}`);;
+
+//     }
+
+// }
+
+// let fruits = ['apple' , 'banana' , 'orange' , 'watermelon' , 'grapes'];
+
+// console.log(fruits);
+// const ul = document.querySelector('ul');
+// for (let i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i]);
+//     ul.innerHTML += `<li>${fruits[i]}</li>`
+// }
+
+
+// const names =['haashir' ,  'shakeel' , 'abdulhabib' ,'suleman','haashir' ,  'shakeel','abdulhabib','suleman']
+// console.log(names);
+// const ul =document.querySelector('ul');
+// for (let i = 0; i < names.length; i++) {
+//    console.log(names[i]);
+//    ul.innerHTML += `<li>${names[i]}</li>`
+
+// const shop = ['banana','apple','mango','grapes','watermelon']
+// const input=document.querySelector('#input')
+// const  h2 =document.querySelector('h2')
+// function getfruits(){
+//     console.log('input.value');
+//     if (shop.includes(input.value)){
+//         console.log('fruits available hai');
+//         h2.innerHTML =`${input.value} is available`
+//     }
+//     else {
+//         console.log('fruits available nahi hai');
+//         h2.innerHTML=`${input.value} is not available`
+//     }
+//     input.value =('')
+// }
+
+// // console.log(names);
+// function names(){
+//     ul.innerHTML = ""
+// console.log(names);
+//     for (let i = 0; i < names.length; i++) {
+//         console.log(names);
+//  ul.innerHTML +=`<li> ${names[i]} </li>`
+//     }
+// }
+// let i = 100
+// while (i <50) {
+//     console.log("while loop ===>", i);
+//     i++
+// }
+// while loop
+// let j = 11;
+// while (j < 10) {
+//     console.log('while loop ===>', j);
+//     j++
+// }
+// do {
+//     console.log('do while loop===>' ,j);
+// } while (j < 10);
+
+//  var x = prompt("Where does the Pope live?");
+//  if (x === "Vatican") {
+//  alert("Correct!");
+//  }
+//  if (yourTicketNumber !== 487208) {
+//      alert("Better luck next time.");
+//      }
+
+
+// > is greater than
+// < is less than
+// >= is greater than or equal to
+// <= is less than or equal to
+// var x = +prompt('where does you number')
+// if (x===87) {
+//     alert('show your number')
+// } else {
+//     alert('bekaar hai bhayya mai to toot gaya')
+// }
+
+
+// if (weight > 300 && time < 6) {
+//      alert("Come to our tryout!");
+//      }
+//      else {
+//      alert("Come to our cookout!");
+//      }
+//      if (weight > 300 && time < 6 && age > 17 && gender === "male") {
+//          alert("Come to our tryout!");
+//          }
+//          else {
+//          alert("Come to our cookout!");
+//          }
+
+// var cities = ['karachi', 'lahore', 'peshawar', 'islamabad']
+// // alert('welcome to', cities[3])
+// // cities.splice(0, 1)
+// console.log(cities);
